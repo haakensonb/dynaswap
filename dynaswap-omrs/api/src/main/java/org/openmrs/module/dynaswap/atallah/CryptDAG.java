0571f9a2d4868800432f503346d0eb707c41c23d
haakensonb <untamedhawk44@gmail.com>
2020-10-22 23:09:46 -0400

    Clean up messy comments and unused code.
    DAG creation is working correctly now for the testcase.

--- dynaswap-omrs/api/src/main/java/org/openmrs/module/dynaswap/atallah/CryptDAG.java
+++ dynaswap-omrs/api/src/main/java/org/openmrs/module/dynaswap/atallah/CryptDAG.java
@@ -48,28 +48,18 @@ public class CryptDAG {
 		this.createGraph();
 	}
 	
-	// private Set<String> getPrivNameSet(Set<Privilege> privSet){
-	// 	Set<String> privNames = new HashSet<String>();
-	// 	for(Privilege priv : privSet){
-	// 		// get privilege names
-	// 		privNames.add(priv.getPrivilege());
-	// 	}
-	// 	return privNames;
-	// }
-	
 	@Transactional
 	public void createGraph() {
 		List<Role> unfilteredRoles = Context.getUserService().getAllRoles();
 		// Filter down to only Roles with the right name prefix.
 		List<Role> roles = this.getPrefixFilteredRoles(unfilteredRoles);
-		// ArrayList<Set<Privilege>> nodePrivs = new ArrayList<Set<Privilege>>();
+		
 		ArrayList<Set<String>> nodePrivs = new ArrayList<Set<String>>();
 		HashMap<String, Privilege> privNameToPrivObjMapping = new HashMap<String, Privilege>();
 		ArrayList<ImmutablePair<String, Set<String>>> roleNameToPrivNamesMappings = new ArrayList<ImmutablePair<String, Set<String>>>();
+		
 		for (Role role : roles) {
 			// Get a list of all the possible privileges.
-			// nodePrivs.add(role.getPrivileges());
-			// Set<String> privNames = this.getPrivNameSet(role.getPrivileges());
 			Set<String> privNames = new HashSet<String>();
 			for (Privilege priv : role.getPrivileges()) {
 				// Get privilege names
@@ -84,6 +74,7 @@ public class CryptDAG {
 			ImmutablePair<String, Set<String>> mapping = new ImmutablePair<String, Set<String>>(roleName, privNames);
 			roleNameToPrivNamesMappings.add(mapping);
 		}
+		
 		// Find all intersections between privileges and generate dummy nodes.
 		int d = 0;
 		for (int i = 0; i < nodePrivs.size(); i++) {
@@ -102,44 +93,6 @@ public class CryptDAG {
 			}
 		}
 		
-		// print for testing
-		System.out.println("roleNameToPrivNamesMappings (node_name): ");
-		for (ImmutablePair<String, Set<String>> pair : roleNameToPrivNamesMappings) {
-			System.out.println("role: " + pair.getKey());
-			for (String s : pair.getValue()) {
-				System.out.print(s + ", ");
-			}
-			System.out.println();
-		}
-		
-		// Sort rolePrivMappings by number of privileges in descending order.
-		// Sort roleNameToPrivNamesMappings by number of privileges in descending order.
-		// Collections.sort(roleNameToPrivNamesMappings, new Comparator<ImmutablePair<String, Set<String>>>() {
-		
-		// 	public int compare(ImmutablePair<String, Set<String>> p1, ImmutablePair<String, Set<String>> p2) {
-		// 		return p2.getValue().size() - p1.getValue().size();
-		// 	}
-		// });
-		
-		// print for testing
-		System.out.println("roleNameToPrivNamesMappings (node_name) AFTER SORT: ");
-		for (ImmutablePair<String, Set<String>> pair : roleNameToPrivNamesMappings) {
-			System.out.println("role: " + pair.getKey());
-			for (String s : pair.getValue()) {
-				System.out.print(s + ", ");
-			}
-			System.out.println();
-		}
-		
-		// Print nodePrivs for testing
-		System.out.println("nodePrivs before sort: ");
-		for (Set<String> np : nodePrivs) {
-			for (String p : np) {
-				System.out.print(p + " ");
-			}
-			System.out.println();
-		}
-		
 		// Sort nodePrivs by number of privileges in descending order.
 		Collections.sort(nodePrivs, new Comparator<Set<String>>() {
 			
@@ -148,22 +101,11 @@ public class CryptDAG {
 			}
 		});
 		
-		// Print nodePrivs for testing
-		System.out.println("nodePrivs after sort: ");
-		for (Set<String> np : nodePrivs) {
-			for (String p : np) {
-				System.out.print(p + " ");
-			}
-			System.out.println();
-		}
-		
 		ArrayList<String> nodeNames = new ArrayList<String>();
 		for (int i = 0; i < nodePrivs.size(); i++) {
 			nodeNames.add("");
 		}
-		// for (ImmutablePair<String, Set<String>> p : roleNameToPrivNamesMappings) {
-		// 	nodeNames.add(p.getKey());
-		// }
+		// Fill nodeNames using correct ordering
 		for (int i = 0; i < nodePrivs.size(); i++) {
 			for (int j = 0; j < nodePrivs.size(); j++) {
 				if (roleNameToPrivNamesMappings.get(i).getValue() == nodePrivs.get(j)) {
@@ -172,77 +114,25 @@ public class CryptDAG {
 			}
 		}
 		
-		// Print nodeNames for testing
-		System.out.println("node names: ");
-		for (String s : nodeNames) {
-			System.out.print(s + ", ");
-		}
-		System.out.println();
-		
 		// Init adjacency matrix.
-		ArrayList<ArrayList<Integer>> adj_mat = new ArrayList<ArrayList<Integer>>(nodePrivs.size());
+		ArrayList<ArrayList<Integer>> adjMat = new ArrayList<ArrayList<Integer>>(nodePrivs.size());
 		// Init ArrayList with empty ArrayLists for use later in dfs.
 		for (int i = 0; i < nodePrivs.size(); i++) {
-			adj_mat.add(new ArrayList<Integer>());
+			adjMat.add(new ArrayList<Integer>());
 		}
+		
+		// Init tot (total privileges) with empty sets for use later in dfs.
 		ArrayList<Set<String>> tot = new ArrayList<Set<String>>(nodePrivs.size());
-		// Init ArrayList with empty sets for use later in dfs.
 		for (int i = 0; i < nodePrivs.size(); i++) {
 			tot.add(Collections.<String> emptySet());
 		}
-		// Print nodePrivs for testing
-		System.out.println("nodePrivs before dfs: ");
-		for (Set<String> np : nodePrivs) {
-			for (String p : np) {
-				System.out.print(p + " ");
-			}
-			System.out.println();
-		}
 		
 		// Use depth first search to build the adjacency matrix.
-		this.dfs(adj_mat, nodePrivs, tot, 0);
-		
-		// Print nodePrivs for testing
-		System.out.println("nodePrivs after dfs: ");
-		for (Set<String> np : nodePrivs) {
-			for (String p : np) {
-				System.out.print(p + " ");
-			}
-			System.out.println();
-		}
-		
-		// // Print nodePrivs for testing
-		// System.out.println("nodePrivs after dfs: ");
-		// for (Set<Privilege> np : nodePrivs) {
-		// 	for (Privilege p : np) {
-		// 		System.out.print(p + " ");
-		// 	}
-		// 	System.out.println();
-		// }
-		
-		// Print tot for testing
-		// System.out.println("tot: ");
-		// for (Set<Privilege> sp : tot) {
-		// 	for (Privilege p : sp) {
-		// 		System.out.print(p + " ");
-		// 	}
-		// 	System.out.println();
-		// }
-		// Print adj mat for testing
-		System.out.println("adj_mat:");
-		String adj_mat_str = "";
-		for (int i = 0; i < adj_mat.size(); i++) {
-			for (int j = 0; j < adj_mat.get(i).size(); j++) {
-				adj_mat_str += adj_mat.get(i).get(j);
-				adj_mat_str += " ";
-			}
-			adj_mat_str += "\n";
-		}
-		System.out.println(adj_mat_str);
+		this.dfs(adjMat, nodePrivs, tot, 0);
 		
 		// Try to store special json formatted version of the graph.
 		try {
-			this.formatGraphAsJson(adj_mat, nodePrivs, nodeNames);
+			this.formatGraphAsJson(adjMat, nodePrivs, nodeNames);
 		}
 		catch (IOException e) {
 			e.printStackTrace();
@@ -262,10 +152,10 @@ public class CryptDAG {
 			nodeMapping.put(name, node);
 		}
 		// Create CryptEdge objects and assign to proper CryptNode objects.
-		for (int i = 0; i < adj_mat.size(); i++) {
+		for (int i = 0; i < adjMat.size(); i++) {
 			int row = i;
-			for (int j = 0; j < adj_mat.get(i).size(); j++) {
-				int val = adj_mat.get(i).get(j);
+			for (int j = 0; j < adjMat.get(i).size(); j++) {
+				int val = adjMat.get(i).get(j);
 				String parentName = nodeNames.get(row);
 				CryptNode parentNode = nodeMapping.get(parentName);
 				String childName = nodeNames.get(val);
@@ -282,23 +172,23 @@ public class CryptDAG {
 		
 	}
 	
-	private void dfs(ArrayList<ArrayList<Integer>> adj_mat, ArrayList<Set<String>> nodePrivs, ArrayList<Set<String>> tot,
+	private void dfs(ArrayList<ArrayList<Integer>> adjMat, ArrayList<Set<String>> nodePrivs, ArrayList<Set<String>> tot,
 	        int cur) {
 		if (cur == nodePrivs.size()) {
 			return;
 		}
 		if (tot.get(cur).size() > 0) {
-			this.dfs(adj_mat, nodePrivs, tot, cur + 1);
+			this.dfs(adjMat, nodePrivs, tot, cur + 1);
 			return;
 		}
 		
 		for (int i = cur + 1; i < nodePrivs.size(); i++) {
-			this.dfs(adj_mat, nodePrivs, tot, i);
+			this.dfs(adjMat, nodePrivs, tot, i);
 			boolean isSubsetOfNodePriv = SetUtils.isProperSubset(tot.get(i), nodePrivs.get(cur));
 			boolean isSubsetOfTotCur = SetUtils.isProperSubset(tot.get(i), tot.get(cur));
 			if (isSubsetOfNodePriv && (!isSubsetOfTotCur)) {
-				if (!adj_mat.get(cur).contains(i)) {
-					adj_mat.get(cur).add(i);
+				if (!adjMat.get(cur).contains(i)) {
+					adjMat.get(cur).add(i);
 				}
 				// union of tot[cur] and tot[i]
 				Set<String> newSet = SetUtils.getUnion(tot.get(cur), tot.get(i));
@@ -306,54 +196,14 @@ public class CryptDAG {
 			}
 		}
 		
-		System.out.println("cur: " + cur);
-		System.out.println("tot before diff: ");
-		for (Set<String> sp : tot) {
-			for (String p : sp) {
-				System.out.print(p + " ");
-			}
-			System.out.println();
-		}
-		
-		// Testing set diff
-		System.out.println("setDiff input setA:");
-		for (String s : nodePrivs.get(cur)) {
-			System.out.print(s + " ");
-		}
-		System.out.println();
-		// Testing set diff
-		System.out.println("setDiff input setB:");
-		for (String s : tot.get(cur)) {
-			System.out.print(s + " ");
-		}
-		System.out.println();
-		
 		Set<String> diffSet = SetUtils.getDifference(nodePrivs.get(cur), tot.get(cur));
-		// Testing set diff
-		System.out.println("nodePriv set before diff:");
-		for (String s : nodePrivs.get(cur)) {
-			System.out.print(s + " ");
-		}
-		System.out.println();
-		
-		System.out.println("diffSet after diff:");
-		for (String s : diffSet) {
-			System.out.print(s + " ");
-		}
-		System.out.println();
-		
 		nodePrivs.set(cur, diffSet);
-		// System.out.println("nodePriv set after diff in arraylist:");
-		// for (String s : nodePrivs.get(cur)) {
-		// 	System.out.print(s + " ");
-		// }
-		// System.out.println();
 		
 		Set<String> unionSet = SetUtils.getUnion(tot.get(cur), nodePrivs.get(cur));
 		tot.set(cur, unionSet);
 	}
 	
-	public void formatGraphAsJson(ArrayList<ArrayList<Integer>> adj_mat, ArrayList<Set<String>> nodePrivs,
+	public void formatGraphAsJson(ArrayList<ArrayList<Integer>> adjMat, ArrayList<Set<String>> nodePrivs,
 	        ArrayList<String> nodeNames) throws IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		ObjectNode info = mapper.createObjectNode();
@@ -361,7 +211,7 @@ public class CryptDAG {
 		ArrayNode nodes = mapper.createArrayNode();
 		ArrayNode edges = mapper.createArrayNode();
 		
-		for (int i = 0; i < adj_mat.size(); i++) {
+		for (int i = 0; i < adjMat.size(); i++) {
 			String nodeName = nodeNames.get(i);
 			// Create json node object.
 			ObjectNode node = mapper.createObjectNode();
@@ -371,10 +221,10 @@ public class CryptDAG {
 			ObjectNode nodeData = mapper.createObjectNode();
 			nodeData.put("data", node);
 			nodes.add(nodeData);
-			for (int j = 0; j < adj_mat.get(i).size(); j++) {
+			for (int j = 0; j < adjMat.get(i).size(); j++) {
 				// Create json edge object.
 				ObjectNode edge = mapper.createObjectNode();
-				int val = adj_mat.get(i).get(j);
+				int val = adjMat.get(i).get(j);
 				String idName = String.format("e%s%s", i, val);
 				edge.put("id", idName);
 				String srcName = nodeNames.get(i);
@@ -396,6 +246,20 @@ public class CryptDAG {
 	
 	public String getFormattedGraph() {
 		return this.formattedGraph;
+	}
+	
+	private void printAdjMat(ArrayList<ArrayList<Integer>> adjMat) {
+		// Print adj mat for testing
+		System.out.println("adjMat:");
+		String adjMatStr = "";
+		for (int i = 0; i < adjMat.size(); i++) {
+			for (int j = 0; j < adjMat.get(i).size(); j++) {
+				adjMatStr += adjMat.get(i).get(j);
+				adjMatStr += " ";
+			}
+			adjMatStr += "\n";
+		}
+		System.out.println(adjMatStr);
 	}
 	
 	private List<Role> getPrefixFilteredRoles(List<Role> unfilteredRoles) {
--- dynaswap-omrs/api/src/test/java/org/openmrs/module/dynaswap/CryptDAGTest.java
+++ dynaswap-omrs/api/src/test/java/org/openmrs/module/dynaswap/CryptDAGTest.java
@@ -20,35 +20,9 @@ public class CryptDAGTest extends BaseModuleContextSensitiveTest {
 		CryptDAG dag = new CryptDAG();
 	}
 	
-	// @Test
-	// public void CryptDAG_priv_equals() {
-	// 	Privilege p1 = new Privilege("1");
-	// 	Privilege p2 = new Privilege("2");
-	// 	boolean eq = p1.equals(p2);
-	// 	System.out.println("p1 == p2: " + eq);
-	// 	Privilege p3 = new Privilege("1");
-	// 	boolean eq2 = p1.equals(p3);
-	// 	System.out.println("p1 == p3: " + eq2);
-	// 	System.out.println("PrivSet test");
-	// 	Set<Privilege> ps = new HashSet<Privilege>();
-	// 	ps.add(p1);
-	// 	System.out.println("PrivSet contains p1: " + ps.contains(p1));
-	// 	System.out.println("PrivSet contains p1 str: " + ps.contains("1"));
-	// }
-	
 	@Test
 	public void CryptDAG_model_gen_simple_mapping() {
 		UserService us = Context.getUserService();
-		// // First clear out existing Roles/Privs
-		// List<Role> roles = us.getAllRoles();
-		// for (Role role : roles) {
-		// 	us.purgeRole(r[CRYPT]ole);
-		// }
-		// List<Privilege> privs = us.getAllPrivileges();
-		// for (Privilege priv : privs) {
-		// 	us.purgePrivilege(priv);
-		// }
-		
 		// Add a simple model generation test case consisting of
 		// the following role-priv object mappings described in a JSON-like
 		// format below. For now, values will be hard coded.
@@ -60,37 +34,31 @@ public class CryptDAGTest extends BaseModuleContextSensitiveTest {
 			privs.add(priv);
 			us.savePrivilege(priv);
 		}
+		
 		Role admin = new Role("[CRYPT]admin");
 		admin.addPrivilege(privs.get(0));
 		admin.addPrivilege(privs.get(1));
 		admin.addPrivilege(privs.get(2));
 		us.saveRole(admin);
+		
 		Role nurse = new Role("[CRYPT]nurse");
 		nurse.addPrivilege(privs.get(1));
 		nurse.addPrivilege(privs.get(2));
 		us.saveRole(nurse);
+		
 		Role clerk = new Role("[CRYPT]clerk");
 		clerk.addPrivilege(privs.get(2));
 		clerk.addPrivilege(privs.get(3));
 		clerk.addPrivilege(privs.get(4));
 		us.saveRole(clerk);
+		
 		Role doctor = new Role("[CRYPT]doctor");
 		doctor.addPrivilege(privs.get(1));
 		doctor.addPrivilege(privs.get(2));
 		doctor.addPrivilege(privs.get(4));
 		us.saveRole(doctor);
+		
 		CryptDAG dag = new CryptDAG();
 		System.out.println(dag.getFormattedGraph());
-		// // Print new roles/privs
-		// List<Role> newRoles = us.getAllRoles();
-		// System.out.println("2nd test");
-		// for (Role role : newRoles) {
-		// 	System.out.println(role);
-		// }
-		// List<Privilege> newPrivs = us.getAllPrivileges();
-		// for (Privilege priv : newPrivs) {
-		// 	System.out.println(priv);
-		// }
-		
 	}
 }
