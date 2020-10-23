fe9b78dd83bcad4d4fcf2efcc35125ed7b905424
haakensonb <untamedhawk44@gmail.com>
2020-10-22 22:40:56 -0400

    Fix SetUtil usage bug in DFS. Graph edges now correct.
    And add a lot of messy comments.

--- dynaswap-omrs/api/src/main/java/org/openmrs/module/dynaswap/atallah/CryptDAG.java
+++ dynaswap-omrs/api/src/main/java/org/openmrs/module/dynaswap/atallah/CryptDAG.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Set;
@@ -47,73 +48,198 @@ public class CryptDAG {
 		this.createGraph();
 	}
 	
+	// private Set<String> getPrivNameSet(Set<Privilege> privSet){
+	// 	Set<String> privNames = new HashSet<String>();
+	// 	for(Privilege priv : privSet){
+	// 		// get privilege names
+	// 		privNames.add(priv.getPrivilege());
+	// 	}
+	// 	return privNames;
+	// }
+	
 	@Transactional
 	public void createGraph() {
 		List<Role> unfilteredRoles = Context.getUserService().getAllRoles();
 		// Filter down to only Roles with the right name prefix.
 		List<Role> roles = this.getPrefixFilteredRoles(unfilteredRoles);
-		ArrayList<Set<Privilege>> nodePrivs = new ArrayList<Set<Privilege>>();
-		// Maybe change role key to use uuid (unique)?
-		ArrayList<ImmutablePair<String, Set<Privilege>>> rolePrivMappings = new ArrayList<ImmutablePair<String, Set<Privilege>>>();
+		// ArrayList<Set<Privilege>> nodePrivs = new ArrayList<Set<Privilege>>();
+		ArrayList<Set<String>> nodePrivs = new ArrayList<Set<String>>();
+		HashMap<String, Privilege> privNameToPrivObjMapping = new HashMap<String, Privilege>();
+		ArrayList<ImmutablePair<String, Set<String>>> roleNameToPrivNamesMappings = new ArrayList<ImmutablePair<String, Set<String>>>();
 		for (Role role : roles) {
 			// Get a list of all the possible privileges.
-			nodePrivs.add(role.getPrivileges());
+			// nodePrivs.add(role.getPrivileges());
+			// Set<String> privNames = this.getPrivNameSet(role.getPrivileges());
+			Set<String> privNames = new HashSet<String>();
+			for (Privilege priv : role.getPrivileges()) {
+				// Get privilege names
+				privNames.add(priv.getPrivilege());
+				// Also populate mapping from privName to actual PrivObject
+				privNameToPrivObjMapping.put(priv.getPrivilege(), priv);
+			}
+			nodePrivs.add(privNames);
 			// Build role/privilege mapping.
 			String roleName = role.getName();
-			Set<Privilege> privs = role.getPrivileges();
-			ImmutablePair<String, Set<Privilege>> mapping = new ImmutablePair<String, Set<Privilege>>(roleName, privs);
-			rolePrivMappings.add(mapping);
+			// Set<Privilege> privs = role.getPrivileges();
+			ImmutablePair<String, Set<String>> mapping = new ImmutablePair<String, Set<String>>(roleName, privNames);
+			roleNameToPrivNamesMappings.add(mapping);
 		}
 		// Find all intersections between privileges and generate dummy nodes.
 		int d = 0;
 		for (int i = 0; i < nodePrivs.size(); i++) {
 			for (int j = i + 1; j < nodePrivs.size(); j++) {
-				Set<Privilege> privs1 = nodePrivs.get(i);
-				Set<Privilege> privs2 = nodePrivs.get(j);
-				Set<Privilege> intersect = SetUtils.getIntersect(privs1, privs2);
+				Set<String> privs1 = nodePrivs.get(i);
+				Set<String> privs2 = nodePrivs.get(j);
+				Set<String> intersect = SetUtils.getIntersect(privs1, privs2);
 				if ((intersect.size() < Math.min(privs1.size(), privs2.size())) && (intersect.size() > 0)
 				        && (nodePrivs.contains(intersect) == false)) {
 					String dummyName = "Placeholder" + Integer.toString(d);
-					ImmutablePair<String, Set<Privilege>> mapping = new ImmutablePair<String, Set<Privilege>>(dummyName,
-					        intersect);
-					rolePrivMappings.add(mapping);
+					ImmutablePair<String, Set<String>> mapping = new ImmutablePair<String, Set<String>>(dummyName, intersect);
+					roleNameToPrivNamesMappings.add(mapping);
 					nodePrivs.add(intersect);
 					d += 1;
 				}
 			}
 		}
-		// Sort rolePrivMappings by number of privileges in descending order.
-		Collections.sort(rolePrivMappings, new Comparator<ImmutablePair<String, Set<Privilege>>>() {
-			
-			public int compare(ImmutablePair<String, Set<Privilege>> p1, ImmutablePair<String, Set<Privilege>> p2) {
-				return p2.getValue().size() - p1.getValue().size();
+		
+		// print for testing
+		System.out.println("roleNameToPrivNamesMappings (node_name): ");
+		for (ImmutablePair<String, Set<String>> pair : roleNameToPrivNamesMappings) {
+			System.out.println("role: " + pair.getKey());
+			for (String s : pair.getValue()) {
+				System.out.print(s + ", ");
 			}
-		});
-		// Place node names in an ArrayList in the same descending order of privileges.
-		ArrayList<String> nodeNames = new ArrayList<String>();
-		for (ImmutablePair<String, Set<Privilege>> p : rolePrivMappings) {
-			nodeNames.add(p.getKey());
+			System.out.println();
 		}
+		
+		// Sort rolePrivMappings by number of privileges in descending order.
+		// Sort roleNameToPrivNamesMappings by number of privileges in descending order.
+		// Collections.sort(roleNameToPrivNamesMappings, new Comparator<ImmutablePair<String, Set<String>>>() {
+		
+		// 	public int compare(ImmutablePair<String, Set<String>> p1, ImmutablePair<String, Set<String>> p2) {
+		// 		return p2.getValue().size() - p1.getValue().size();
+		// 	}
+		// });
+		
+		// print for testing
+		System.out.println("roleNameToPrivNamesMappings (node_name) AFTER SORT: ");
+		for (ImmutablePair<String, Set<String>> pair : roleNameToPrivNamesMappings) {
+			System.out.println("role: " + pair.getKey());
+			for (String s : pair.getValue()) {
+				System.out.print(s + ", ");
+			}
+			System.out.println();
+		}
+		
+		// Print nodePrivs for testing
+		System.out.println("nodePrivs before sort: ");
+		for (Set<String> np : nodePrivs) {
+			for (String p : np) {
+				System.out.print(p + " ");
+			}
+			System.out.println();
+		}
+		
 		// Sort nodePrivs by number of privileges in descending order.
-		Collections.sort(nodePrivs, new Comparator<Set<Privilege>>() {
+		Collections.sort(nodePrivs, new Comparator<Set<String>>() {
 			
-			public int compare(Set<Privilege> p1, Set<Privilege> p2) {
+			public int compare(Set<String> p1, Set<String> p2) {
 				return p2.size() - p1.size();
 			}
 		});
+		
+		// Print nodePrivs for testing
+		System.out.println("nodePrivs after sort: ");
+		for (Set<String> np : nodePrivs) {
+			for (String p : np) {
+				System.out.print(p + " ");
+			}
+			System.out.println();
+		}
+		
+		ArrayList<String> nodeNames = new ArrayList<String>();
+		for (int i = 0; i < nodePrivs.size(); i++) {
+			nodeNames.add("");
+		}
+		// for (ImmutablePair<String, Set<String>> p : roleNameToPrivNamesMappings) {
+		// 	nodeNames.add(p.getKey());
+		// }
+		for (int i = 0; i < nodePrivs.size(); i++) {
+			for (int j = 0; j < nodePrivs.size(); j++) {
+				if (roleNameToPrivNamesMappings.get(i).getValue() == nodePrivs.get(j)) {
+					nodeNames.set(j, roleNameToPrivNamesMappings.get(i).getKey());
+				}
+			}
+		}
+		
+		// Print nodeNames for testing
+		System.out.println("node names: ");
+		for (String s : nodeNames) {
+			System.out.print(s + ", ");
+		}
+		System.out.println();
+		
 		// Init adjacency matrix.
 		ArrayList<ArrayList<Integer>> adj_mat = new ArrayList<ArrayList<Integer>>(nodePrivs.size());
 		// Init ArrayList with empty ArrayLists for use later in dfs.
 		for (int i = 0; i < nodePrivs.size(); i++) {
 			adj_mat.add(new ArrayList<Integer>());
 		}
-		ArrayList<Set<Privilege>> tot = new ArrayList<Set<Privilege>>(nodePrivs.size());
+		ArrayList<Set<String>> tot = new ArrayList<Set<String>>(nodePrivs.size());
 		// Init ArrayList with empty sets for use later in dfs.
 		for (int i = 0; i < nodePrivs.size(); i++) {
-			tot.add(Collections.<Privilege> emptySet());
+			tot.add(Collections.<String> emptySet());
 		}
+		// Print nodePrivs for testing
+		System.out.println("nodePrivs before dfs: ");
+		for (Set<String> np : nodePrivs) {
+			for (String p : np) {
+				System.out.print(p + " ");
+			}
+			System.out.println();
+		}
+		
 		// Use depth first search to build the adjacency matrix.
 		this.dfs(adj_mat, nodePrivs, tot, 0);
+		
+		// Print nodePrivs for testing
+		System.out.println("nodePrivs after dfs: ");
+		for (Set<String> np : nodePrivs) {
+			for (String p : np) {
+				System.out.print(p + " ");
+			}
+			System.out.println();
+		}
+		
+		// // Print nodePrivs for testing
+		// System.out.println("nodePrivs after dfs: ");
+		// for (Set<Privilege> np : nodePrivs) {
+		// 	for (Privilege p : np) {
+		// 		System.out.print(p + " ");
+		// 	}
+		// 	System.out.println();
+		// }
+		
+		// Print tot for testing
+		// System.out.println("tot: ");
+		// for (Set<Privilege> sp : tot) {
+		// 	for (Privilege p : sp) {
+		// 		System.out.print(p + " ");
+		// 	}
+		// 	System.out.println();
+		// }
+		// Print adj mat for testing
+		System.out.println("adj_mat:");
+		String adj_mat_str = "";
+		for (int i = 0; i < adj_mat.size(); i++) {
+			for (int j = 0; j < adj_mat.get(i).size(); j++) {
+				adj_mat_str += adj_mat.get(i).get(j);
+				adj_mat_str += " ";
+			}
+			adj_mat_str += "\n";
+		}
+		System.out.println(adj_mat_str);
+		
 		// Try to store special json formatted version of the graph.
 		try {
 			this.formatGraphAsJson(adj_mat, nodePrivs, nodeNames);
@@ -156,8 +282,8 @@ public class CryptDAG {
 		
 	}
 	
-	private void dfs(ArrayList<ArrayList<Integer>> adj_mat, ArrayList<Set<Privilege>> nodePrivs,
-	        ArrayList<Set<Privilege>> tot, int cur) {
+	private void dfs(ArrayList<ArrayList<Integer>> adj_mat, ArrayList<Set<String>> nodePrivs, ArrayList<Set<String>> tot,
+	        int cur) {
 		if (cur == nodePrivs.size()) {
 			return;
 		}
@@ -168,24 +294,66 @@ public class CryptDAG {
 		
 		for (int i = cur + 1; i < nodePrivs.size(); i++) {
 			this.dfs(adj_mat, nodePrivs, tot, i);
-			boolean nodePrivIsSubset = SetUtils.isProperSubset(nodePrivs.get(cur), tot.get(i));
-			boolean totIsSubset = SetUtils.isProperSubset(tot.get(cur), tot.get(i));
-			if (nodePrivIsSubset && (!totIsSubset)) {
+			boolean isSubsetOfNodePriv = SetUtils.isProperSubset(tot.get(i), nodePrivs.get(cur));
+			boolean isSubsetOfTotCur = SetUtils.isProperSubset(tot.get(i), tot.get(cur));
+			if (isSubsetOfNodePriv && (!isSubsetOfTotCur)) {
 				if (!adj_mat.get(cur).contains(i)) {
 					adj_mat.get(cur).add(i);
 				}
 				// union of tot[cur] and tot[i]
-				Set<Privilege> newSet = SetUtils.getIntersect(tot.get(cur), tot.get(i));
+				Set<String> newSet = SetUtils.getUnion(tot.get(cur), tot.get(i));
 				tot.set(cur, newSet);
 			}
 		}
-		Set<Privilege> diffSet = SetUtils.getDifference(nodePrivs.get(cur), tot.get(cur));
+		
+		System.out.println("cur: " + cur);
+		System.out.println("tot before diff: ");
+		for (Set<String> sp : tot) {
+			for (String p : sp) {
+				System.out.print(p + " ");
+			}
+			System.out.println();
+		}
+		
+		// Testing set diff
+		System.out.println("setDiff input setA:");
+		for (String s : nodePrivs.get(cur)) {
+			System.out.print(s + " ");
+		}
+		System.out.println();
+		// Testing set diff
+		System.out.println("setDiff input setB:");
+		for (String s : tot.get(cur)) {
+			System.out.print(s + " ");
+		}
+		System.out.println();
+		
+		Set<String> diffSet = SetUtils.getDifference(nodePrivs.get(cur), tot.get(cur));
+		// Testing set diff
+		System.out.println("nodePriv set before diff:");
+		for (String s : nodePrivs.get(cur)) {
+			System.out.print(s + " ");
+		}
+		System.out.println();
+		
+		System.out.println("diffSet after diff:");
+		for (String s : diffSet) {
+			System.out.print(s + " ");
+		}
+		System.out.println();
+		
 		nodePrivs.set(cur, diffSet);
-		Set<Privilege> unionSet = SetUtils.getUnion(tot.get(cur), nodePrivs.get(cur));
+		// System.out.println("nodePriv set after diff in arraylist:");
+		// for (String s : nodePrivs.get(cur)) {
+		// 	System.out.print(s + " ");
+		// }
+		// System.out.println();
+		
+		Set<String> unionSet = SetUtils.getUnion(tot.get(cur), nodePrivs.get(cur));
 		tot.set(cur, unionSet);
 	}
 	
-	public void formatGraphAsJson(ArrayList<ArrayList<Integer>> adj_mat, ArrayList<Set<Privilege>> nodePrivs,
+	public void formatGraphAsJson(ArrayList<ArrayList<Integer>> adj_mat, ArrayList<Set<String>> nodePrivs,
 	        ArrayList<String> nodeNames) throws IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		ObjectNode info = mapper.createObjectNode();
--- dynaswap-omrs/api/src/test/java/org/openmrs/module/dynaswap/CryptDAGTest.java
+++ dynaswap-omrs/api/src/test/java/org/openmrs/module/dynaswap/CryptDAGTest.java
@@ -2,6 +2,8 @@ package org.openmrs.module.dynaswap;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 
 import org.junit.Test;
 import org.openmrs.module.dynaswap.atallah.CryptDAG;
@@ -17,6 +19,22 @@ public class CryptDAGTest extends BaseModuleContextSensitiveTest {
 	public void CryptDAG_constructor() {
 		CryptDAG dag = new CryptDAG();
 	}
+	
+	// @Test
+	// public void CryptDAG_priv_equals() {
+	// 	Privilege p1 = new Privilege("1");
+	// 	Privilege p2 = new Privilege("2");
+	// 	boolean eq = p1.equals(p2);
+	// 	System.out.println("p1 == p2: " + eq);
+	// 	Privilege p3 = new Privilege("1");
+	// 	boolean eq2 = p1.equals(p3);
+	// 	System.out.println("p1 == p3: " + eq2);
+	// 	System.out.println("PrivSet test");
+	// 	Set<Privilege> ps = new HashSet<Privilege>();
+	// 	ps.add(p1);
+	// 	System.out.println("PrivSet contains p1: " + ps.contains(p1));
+	// 	System.out.println("PrivSet contains p1 str: " + ps.contains("1"));
+	// }
 	
 	@Test
 	public void CryptDAG_model_gen_simple_mapping() {
@@ -62,6 +80,7 @@ public class CryptDAGTest extends BaseModuleContextSensitiveTest {
 		doctor.addPrivilege(privs.get(4));
 		us.saveRole(doctor);
 		CryptDAG dag = new CryptDAG();
+		System.out.println(dag.getFormattedGraph());
 		// // Print new roles/privs
 		// List<Role> newRoles = us.getAllRoles();
 		// System.out.println("2nd test");
